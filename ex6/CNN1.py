d4401e22e7790ba9d888dd2a854a4c285d254af7
pham duy <phamduyhk@gmail.com>
2019-07-22 14:57:14 +0900

    ex6

--- ex6/CNN1.py
+++ ex6/CNN1.py
@@ -10,64 +10,116 @@ import seaborn as sns
 np.random.seed(123)
 
 #####
+
+
 def ReLU(x):
-    return x*(x>0)
+    return x*(x > 0)
+
 
 def Convolution(img, param, bias, fncs, channel, padding, stride):
-    ##### 課題1-(a). 畳み込み層の計算を完成させる
-    pad = np.pad(img, [(0, 0), (0, 0), (padding, padding),
-                       (padding, padding)], 'constant')
-    H,W = img.shape
-    filter_h = param.shape[0]
-    filter_w = param.shape[1]
-    out_h = (H + 2*pad - filter_h)//stride + 1
-    out_w = (W + 2*pad - filter_w)//stride + 1
+    """
+    @param
+    img.shape = (channel, h,w)
+    param.shape = (channel,h,w)
+    bias.shape = (channel)
+    fncs: ReLU, sigmoid, softmax ...
 
-    conv = np.zeros((1, channel, filter_h, filter_w, out_h, out_w))
-    
-    return 
+    @return
+    convolution matrix with shape = (channel,height,weight)
+    """
+    ##### 課題1-(a). 畳み込み層の計算を完成させる
+    image_padded = np.pad(img, [(0, 0), (padding, padding),
+                                (padding, padding)], 'constant')
+    H, W = img.shape[1], img.shape[2]
+    filter_h = param.shape[1]
+    filter_w = param.shape[2]
+    out_h = (H + 2*padding - filter_h)//stride + 1
+    out_w = (W + 2*padding - filter_w)//stride + 1
+    conv = np.zeros((channel, out_h, out_w))
+    for c in range(channel):
+        for i in range(out_h):
+            for j in range(out_w):
+                for p in range(filter_h):
+                    for q in range(filter_w):
+                        conv[c, i, j] += param[c, p, q] * image_padded[c,
+                                                                       stride * (i-1)+p, stride*(j-1)+q]+bias[c]
+    conv = fncs(conv)
+    return conv
+
 
 def MaxPooling(img, filter_size, channel, stride):
+    """
+    @param
+    img.shape = (channel, h,w)
+    filter.height = filter.weight = filter_size
+
+    @return
+    convolution matrix with shape = (channel,height,weight)
+    """
     ##### 課題1-(b). maxプーリングの計算を完成させる
-    
-    return 
+    H, W = img.shape[1], img.shape[2]
+    out_h = (H - filter_size)//stride + 1
+    out_w = (W - filter_size)//stride + 1
+    P = np.zeros((channel, out_h, out_w))
+    for c in range(channel):
+        for i in range(out_h):
+            for j in range(out_w):
+                inside_filter = []
+                for p in range(filter_size):
+                    for q in range(filter_size):
+                        inside_filter.append(
+                            img[c, stride*(i-1)+p, stride*(j-1)+q])
+                P[c, i, j] = max(inside_filter)
+    return P
 
 
-##### データの読み込み
+# データの読み込み
 img = np.load("./img.npy")
 d = img.shape[0]
 
-# ##### 課題2-(a)
-# w = 
+# 課題2-(a)
+W1 = np.array([[-1, -1, 0],
+               [-1, 0, 1],
+               [0, 1, 1]])
+W2 = np.array([[0, 1, 0],
+               [1, -4, 1],
+               [0, 1, 0]])
+W3 = np.ones((3, 3))
+w = np.array([W1, W2, W3])
 
-# ##### 課題2-(b)
-# ##### 畳み込み
-# C = 
+# 課題2-(b)
+# 畳み込み
+img_with_3channel = np.array([img, img, img])
+print("Img shape {}".format(img_with_3channel.shape))
+bias = np.zeros((3))
+C = Convolution(img_with_3channel, w, bias, ReLU, 3, 2, 1)
+print("Convolution shape {}".format(C.shape))
 
-# ##### プーリング
-# P =    
+# プーリング
+P = MaxPooling(C, 3, 3, 3)
+print("Pooling shape {}".format(P.shape))
 
-# ########## 結果の出力
-# plt.clf()
-# sns.heatmap(img, cbar =False, cmap="CMRmap", square=True)
-# plt.axis("off")
-# plt.savefig("./original.pdf", bbox_inches='tight', transparent = True)
+# transpose C and P for output (if using format (height, width, channel))
+# C = C.T
+# P = P.T
+# 結果の出力
+plt.clf()
+sns.heatmap(img, cbar=False, cmap="CMRmap", square=True)
+plt.axis("off")
+plt.savefig("./original.pdf", bbox_inches='tight', transparent=True)
 
-# for i in range(C.shape[2]):
-#     plt.clf()
-#     sns.heatmap(C[:,:,i], cbar =False, cmap="CMRmap", square=True)
-#     plt.axis("off")
-#     plt.title('Convolution1: axis{}'.format(i))
-#     plt.savefig("./conv1-{}.pdf".format(i), bbox_inches='tight', transparent = True)
+for i in range(C.shape[0]):
+    plt.clf()
+    sns.heatmap(C[i, :, :], cbar=False, cmap="CMRmap", square=True)
+    plt.axis("off")
+    plt.title('Convolution1: axis{}'.format(i))
+    plt.savefig("./conv1-{}.pdf".format(i),
+                bbox_inches='tight', transparent=True)
 
-# for i in range(P.shape[2]):
-#     plt.clf()
-#     sns.heatmap(P[:,:,i], cbar =False, cmap="CMRmap", square=True)
-#     plt.axis("off")
-#     plt.title('Pooling: axis{}'.format(i))
-#     plt.savefig("./pooling{}.pdf".format(i), bbox_inches='tight', transparent = True)    
-
-
-
-
-
+for i in range(P.shape[0]):
+    plt.clf()
+    sns.heatmap(P[i, :, :], cbar=False, cmap="CMRmap", square=True)
+    plt.axis("off")
+    plt.title('Pooling: axis{}'.format(i))
+    plt.savefig("./pooling{}.pdf".format(i),
+                bbox_inches='tight', transparent=True)
